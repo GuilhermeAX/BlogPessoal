0cf49397d55ad5df3a8cbf51ae0646986a29acb5
Guilherme A. Silva <guilherme.salmeida@mercadolivre.com>
2022-12-02 14:05:30 -0300

    implementação dos metodos Listar todos e Buscar por ID

--- BlogPessoal/BlogPessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ BlogPessoal/BlogPessoal/src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.generation.blogpessoal.controller;
 
 import com.generation.blogpessoal.model.Usuario;
 import com.generation.blogpessoal.model.UsuarioLogin;
+import com.generation.blogpessoal.repository.UsuarioRepository;
 import com.generation.blogpessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +20,9 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @PostMapping("/cadastrar")
     public ResponseEntity<Usuario> postUsuario(@Valid @RequestBody Usuario usuario) {
@@ -38,5 +43,17 @@ public class UsuarioController {
         return usuarioService.autenticarUsuario(usuarioLogin)
                 .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
                 .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+    }
+
+    @GetMapping("/listartodos")
+    public ResponseEntity<List<Usuario>> listarTodos(){
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("buscarporid/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
